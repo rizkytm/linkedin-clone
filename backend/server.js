f292ee878b5579a4a936b12dad2ab26ab8f49664
rizkytm <ritrime97@gmail.com>
2025-01-19 12:41:51 +0700

    feat: build app

--- backend/server.js
+++ backend/server.js
@@ -8,18 +8,23 @@ import notificationRoutes from './routes/notification.route.js';
 import connectionRoutes from './routes/connection.route.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  })
-);
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+      credentials: true,
+    })
+  );
+}
+
 app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 
@@ -28,6 +33,14 @@ app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/notifications', notificationRoutes);
 app.use('/api/v1/connections', connectionRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
